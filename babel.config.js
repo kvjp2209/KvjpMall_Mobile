7060b2c4a9aa1d30961ccb1a1918d04bd2ab4d33
Nguyễn Kiên <nguyenthekien20011@gmail.com>
2024-09-05 18:00:09 +0700

    feat: setup eslint, prettier, ts

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        cwd: 'babelrc',
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        root: ['./src'],
+        alias: {
+          '*': './src', // remove this after refactor to use @foo
+        },
+      },
+    ],
+  ],
 };
